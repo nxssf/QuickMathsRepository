badc11ea9789b929cb5391f1c4102c88c9ea68ce
nxsff <vanessafeng881@gmail.com>
2019-04-17 21:47:40 +1000

    commit

--- app/src/main/java/com/example/quickmaths/ChoiceActivity.java
+++ app/src/main/java/com/example/quickmaths/ChoiceActivity.java
@@ -1,4 +1,7 @@
 package com.example.quickmaths;
 
+/*This activity is the second screen presented to users.
+It contains 3 buttons: 'Practise', 'Quiz', and 'View Notes'.*/
+
 public class ChoiceActivity {
 }
--- app/src/main/java/com/example/quickmaths/MainActivity.java
+++ app/src/main/java/com/example/quickmaths/MainActivity.java
@@ -3,6 +3,10 @@ package com.example.quickmaths;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/*This is the screen users are initially presented with.
+It contains 2 buttons: 'Start' and 'Additional Resources'.
+The 'Start' button will bring users to ChoiceActivity.*/
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
