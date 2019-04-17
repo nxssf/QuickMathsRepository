0b5f9c53300cd5a487dcfe045229c6c43864b1e1
nxsff <vanessafeng881@gmail.com>
2019-04-18 00:00:07 +1000

    commit

--- app/src/main/java/com/example/quickmaths/QuizActivity.java
+++ app/src/main/java/com/example/quickmaths/QuizActivity.java
@@ -2,6 +2,16 @@ package com.example.quickmaths;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.quickmaths.Entities.QuestionDBResponse;
+import com.example.quickmaths.Entities.QuestionService;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Call;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class QuizActivity extends AppCompatActivity {
 
@@ -9,5 +19,22 @@ public class QuizActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
+    }
+
+    public void updateQuestion(View view) {
+        requestQuestion();
+    }
+
+    public void requestQuestion() {
+
+        Gson gson = new GsonBuilder().create();
+
+        QuestionService service = new Retrofit.Builder().baseUrl("https://math.ly/api/v1/algebra/linear-equations.json?difficulty=beginner")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build()
+                .create(QuestionService.class);
+
+        // Call<QuestionDBResponse> call = service.getClass();
+
     }
 }
